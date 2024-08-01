accc9683ea2e5854061ddffc85dfa65b2f19bc0e
dustin <dustin@ootri.com>
2024-08-01 16:35:34 -0600

    fixed const.py file

--- custom_components/unifi_rule_management/const.py
+++ custom_components/unifi_rule_management/const.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-"""Constants for Kwikset Monitoring"""
+"""Constants for UniFi Rule Management"""
 import logging
 
 #LOGGER = logging.getLogger(__package__)
-
-DOMAIN = "unifi"
-MANUFACTURER = "Ubiquiti Networks"
-=======
-"""Constants for UniFi rules manager"""
-import logging
-
-#LOGGER = logging.getLogger(__package__)
-#ln -s $(pwd)/ootri-unifi_rule_management/custom_components/unifi_rule_management ./custom_components/unifi_rule_management
 
 # DOMAIN = "unifi"
 # MANUFACTURER = "Ubiquiti Networks"
->>>>>>> e1e77aa (Re-initialize repository)
